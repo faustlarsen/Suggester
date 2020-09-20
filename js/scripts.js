bd33b3fab2be29f3e324b3a0bb09998491f6005e
faustlarsen <faustlarsen@gmail.com>
2020-09-19 19:58:22 -0700

    add questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#language").submit(function(event) {
+    
     event.preventDefault();
 
     const aa = $("input:radio[name=aa]:checked").val();
@@ -8,29 +9,27 @@ $(document).ready(function() {
     const dd = $("input:radio[name=dd]:checked").val();
     const ee = $("input:radio[name=ee]:checked").val();
 
-
   if (aa === 'a' && bb === 'd' && cc === 'g' && dd === 'j' && ee === 'm') {
-    $("#result1").show();
+    $("#result1").fadeIn();
     $("#result2").hide();
     $("#result3").hide();
     $("#result4").hide();
-
+   
   } else if (aa === 'b' && bb === 'e' && cc === 'h' && dd === 'k' && ee === 'n') {
-    $("#result2").show();
+    $("#result2").fadeIn();
     $("#result1").hide();
     $("#result3").hide();
     $("#result4").hide();
 
   } else if (aa === 'c' && bb === 'f' && cc === 'i' && dd === 'l' && ee === 'o') {
-    $("#result3").show();
+    $("#result3").fadeIn();
     $("#result1").hide();
     $("#result2").hide();
     $("#result4").hide();
 
   } else 
-    $("#result4").show();
-  
-    
+    $("#result4").fadeIn()
+
   });
 });
 
